7a6af851d18590331876a0827d5d00f1d6dcd45c
Diego Andres Vasquez Avila <davasqueza@unal.edu.co>
2016-04-27 22:58:23 -0500

    Validación de formulario y conexión con backend

--- src/app/user/registerUser.controller.js
+++ src/app/user/registerUser.controller.js
@@ -4,10 +4,23 @@
 	angular
 		.module('ingeSoftIi')
 		.controller('RegisterUserController', RegisterUserController);
-
-	function RegisterUserController(UserService) {
+	
+	/** @ngInject */
+	function RegisterUserController(UserService, toastr) {
 		var vm = this;
 		
+		vm.profile = {gender: "M", admin: false};
 		vm.genders = UserService.getAllGenders();
+		vm.registerUser = registerUser;
+		
+		function registerUser(){
+			UserService.createUser(vm.profile)
+				.then(function(){
+					toastr.success('El usuario fue registrado con exito', 'Oferta creada');
+				})
+				.catch(function(){
+					toastr.error('Hubo un error al intentar registrar el usuario!', 'Error!');
+				});
+		}
 	}
 })();
