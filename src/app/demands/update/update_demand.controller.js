fb0a8908e249e2f016a1683da513950048442e22
gggonzalezg <gggonzalezg@unal.edu.co>
2016-05-06 00:35:55 -0500

    crud(PLANTILLA EDITAR DEMANDA): a revisión y en espera de comentarios acerca de la plantilla.

---
+++ src/app/demands/update/update_demand.controller.js
@@ -0,0 +1,11 @@
+(function () {
+    'use strict';
+    angular
+      .module('ingeSoftIi')
+      .controller('UpdateDemandController',UpdateDemandController);
+
+    function UpdateDemandController() {
+      var vm = this;
+      vm.switch = true;
+    }
+})();
